c2924568cec22c372fc8ebb45068175cdb579ced
Kevin0626 <1138849013@qq.com>
2023-01-21 22:51:47 +0800

    add project ch05-1 (#36)
    * add project ch05-1
    
    * add project ch05-1
    
    * ch05-1: add main program and globals
    
    * add ch08-2 modbus example
    
    * add ch08-3 ethercat example
    
    * ch08 io map update
    
    * change ch08 to ch07
    
    * change ch0803 to ch07-3
    
    * simplify ch07-2
    
    * add ch09: servo driver
    
    * readme update
    
    * add ch08: motion control
    
    * ch07-3: add readme
    
    * readme update
    
    * readme update
    
    * readme update
    
    * ch05-1: add resource
    
    * add ch07-4 for dm3e-556 servo
    
    * ch07-4 update
    
    * update ch08 sample code
    
    * ch05-1 fix
    
    * ch07-2: change the test config for is620n slave id from 2 to 20
    
    * ch7-3 update: add some code for another servo
    
    * ch7-3: add script to install config
    
    * update ch7-3 README
    
    * ch7-2: change mb to modbus
    
    * remove test() from ch06
    
    * Add document for open project from menu
    
    * update readme for ch704
    
    * add ch07-5
    
    * update
    
    * ch705

---
+++ projects/ch07-5/TOOLS/test.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+import sys
+import time
+import random
+import os
+import json
+import argparse
+import modbus_tk
+import modbus_tk.defines as cst
+from modbus_tk import modbus_tcp
+import logging
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Modbus test server validator")
+    parser.add_argument('-p', dest = 'port', action = 'store',
+        default = 1502,
+        help = 'modbus device port number')
+    parser.add_argument('-s', dest = 'step', action = 'store_true',
+        default = False,
+        help = 'Wait input for each round')    
+    args = parser.parse_args()
+
+    dirname, filename = os.path.split(os.path.abspath(sys.argv[0]))
+    port = int(args.port)
+    fc_read = cst.READ_COILS
+    fc_write = cst.WRITE_MULTIPLE_COILS
+    logger = modbus_tk.utils.create_logger("console", level=logging.DEBUG)
+
+    os.system('kill -9 `pidof modbus_test_server`');
+    os.system(f'/ams/iagent/product/modbus_test_server -p {port} &')
+    os.system(f'/ams/iagent/product/modbus_test_server -p {port+1} &')
+
+    time.sleep(2)
+
+    try:
+        # Connect to the slave
+        master = modbus_tcp.TcpMaster('127.0.0.1', port)
+        master.set_timeout(5.0)
+        master2 = modbus_tcp.TcpMaster('127.0.0.1', port)
+        master2.set_timeout(5.0)
+
+    except modbus_tk.modbus.ModbusError as exc:
+        logger.error("%s- Code=%d", exc, exc.get_exception_code())
+        sys.exit("connect fail")
+
+    logger.info("tool: connected to modbus device")
+
+    v1 = 1
+    v2 = 0
+    result=''
+    while True:
+        try:
+            master.execute(1,cst.WRITE_SINGLE_REGISTER, 100, output_value = v1)
+            master.execute(1,cst.WRITE_MULTIPLE_REGISTERS, 101, output_value = [v1, v1, v1])
+            master.execute(1,cst.WRITE_SINGLE_COIL, 80, output_value = v2)
+
+            result = master.execute(1,cst.READ_HOLDING_REGISTERS, 100, 1)
+            logger.info('read:' + str(result))
+            time.sleep(2)
+            
+            result = master2.execute(1,cst.READ_HOLDING_REGISTERS, 100, 1)
+            logger.info(result)
+            if (result[0] != v1):
+                sys.exit(f'read: {result}, not match prevoius write: {v1}')
+
+            result = master2.execute(1,cst.READ_HOLDING_REGISTERS, 101, 3)
+            logger.info(result)
+            if (result != (v1, v1, v1)):
+                sys.exit(f'read: {result}, not match prevoius write: {v1}')
+
+            result = master2.execute(1,cst.READ_COILS, 80, 1)
+            logger.info(result)
+            if result[0] != v2:
+                sys.exit(f'read: {result}, not match prevoius write: {v2}')
+            v1 = v1 + 1
+            v2 = 1 - v2
+
+        except modbus_tk.modbus.ModbusError as exc:
+            logger.error("%s- Code=%d", exc, exc.get_exception_code())
+        except SystemExit as exc:
+            print (f'{exc}, exiting...')
+            break
+        except Exception as exc:
+            logger.error("Except:%s", exc)
+        # 
+        if args.step:
+            input(f'continue?')
